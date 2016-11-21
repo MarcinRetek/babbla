ba5b0d03147c0e874acb58067719b03c85e49209
Marlon Jakobsson <Marlon Jakobsson>
2016-11-21 12:54:46 +0100

    fix menu

---
+++ WebContent/resources/JS/login-script.js
@@ -0,0 +1,28 @@
+
+function onSignIn(googleUser) {
+
+    var profile = googleUser.getBasicProfile();
+
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Email: ' + profile.getEmail());
+
+    var user = {
+        username: profile.getName(),
+        email: profile.getEmail()
+    }
+
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+
+
+
+
+
