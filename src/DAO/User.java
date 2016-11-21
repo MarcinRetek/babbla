8698ae8e619b503e82e55014d480a6223a37cd34
Johannes Klint <johannesklint@gmail.com>
2016-11-21 10:53:18 +0100

    add save to DB

--- src/DAO/User.java
+++ src/DAO/User.java
@@ -1,5 +1,7 @@
 package DAO;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +11,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="users")
-public class User {
+public class User implements Serializable{
 	
 	@Id
 	@Column(unique=true, nullable=false)
--- src/DAO/UserDAO.java
+++ src/DAO/UserDAO.java
@@ -1,16 +1,17 @@
 package DAO;
 
+import javax.ejb.Stateful;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import EJB.LocalUser;
 
-public class UserDAO implements LocalUser{
+@Stateful
+public class UserDAO{
 
 	@PersistenceContext
 	EntityManager entityManager;
 	
-	@Override
 	public User saveUser(User user) {
 		entityManager.merge(user);
 		return user;
